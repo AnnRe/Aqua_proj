f6d85899f7b99b7bc45ed77ed2abc3719f7eca67
Magdalena <magdalena.pachalska@gmail.com>
2014-10-28 13:28:47 +0100

    no message

---
+++ aquadrom/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+
+namespace aquadrom
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection polaczenie;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            polaczenie = new SqlConnection();
+            polaczenie.ConnectionString = aquadrom.Properties.Settings.Default.AquadromConnectionString;
+            polaczenie.Open();
+           
+            string sql = "SELECT * FROM Pracownik";
+            SqlCommand cmdsel = new SqlCommand(sql, polaczenie);
+            DataTable dt3 = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(sql, polaczenie);
+            da.Fill(dt3);
+
+            dataGridView1.DataSource = dt3.DefaultView;
+          
+        }
+    }
+}
