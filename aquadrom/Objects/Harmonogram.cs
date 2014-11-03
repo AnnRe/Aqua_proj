e2bfbd117d4c136461e0c0eaef94e1b8bbacf49e
Ania <lady_anya@o2.pl>
2014-11-03 18:47:59 +0100

    Harmonogram dodany

---
+++ aquadrom/Objects/Harmonogram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Objects;
+
+namespace aquadrom.Objects
+{
+    public class Harmonogram
+    {
+
+        public void Add(Pracownik pracownik, DateTime pocz, DateTime koniec)
+        {
+            if (PoprawnieRozdzieloneGodziny() && PoprawnieRozplanowanyDzien())
+            { 
+            }
+            //TODO
+        }
+
+        /// <summary>
+        /// Sprawdza, czy w każdym momencie jest wystarczająca ilość pracowników oraz czy są KZ/KSR
+        /// </summary>
+        /// <returns></returns>
+        private bool PoprawnieRozplanowanyDzien()
+        {
+            //TODO: sprawdzenie po godzinie, 
+            //TODO: sprawdzenie KZ,KSR
+            return true;
+        }
+
+        /// <summary>
+        /// Sprawdza czy pracownicy mają wypełnioną pulę godzin
+        /// </summary>
+        /// <returns></returns>
+        private bool PoprawnieRozdzieloneGodziny()
+        {
+            //TODO
+            return true;
+        }
+    
+    }
+}
