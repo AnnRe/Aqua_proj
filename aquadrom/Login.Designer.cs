368c659da418382e04fd7f2aa79c8be68f81445e
Sebastian Nalepka <seba992@gmail.com>
2014-11-18 12:16:09 +0100

    - Login -> AdminPanel, UserPanel - database.sql - > Dodanie kolumny Typ_logowania + drobne zmiany

--- aquadrom/Login.Designer.cs
+++ aquadrom/Login.Designer.cs
@@ -95,6 +95,8 @@
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Logowanie";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Login_FormClosing);
+            this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
