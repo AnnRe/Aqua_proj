c4a6ed5b2fb7710c38be7bd451495c288a6f5582
Ania <lady_anya@o2.pl>
2014-11-18 20:14:34 +0100

    Dodanie plików do repo

---
+++ aquadrom/Test.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using DB;
+
+
+namespace aquadrom
+{
+    public partial class Test : Form
+    {
+        SqlConnection polaczenie;
+       public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void Test_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("TEST");
+            polaczenie = new SqlConnection();
+            polaczenie.ConnectionString = aquadrom.Properties.Settings.Default.AquadromConnectionString;
+            polaczenie.Open();
+            string sql = "SELECT * FROM Pracownik";
+            DataTable dt3 = new DataTable();
+
+            try
+            {
+                SqlCommand cmdsel = new SqlCommand(sql, polaczenie);
+                SqlDataAdapter da = new SqlDataAdapter(sql, polaczenie);
+                da.Fill(dt3);
+                dataGridView1.DataSource = dt3.DefaultView;
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                MessageBox.Show("Błąd :" + ex.Message);
+            }
+            //DBAdapter adapter = new DBAdapter();
+            //DataTable table = adapter.GetData(sql);
+            //dataGridView1.DataSource = table.DefaultView;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dodajPracownikaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 Form = new Form2();
+            Form.Show();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
