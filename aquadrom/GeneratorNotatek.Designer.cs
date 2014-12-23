f4e5e96ace1262cd131567881f13b3931015b7a8
mateusz.ogiermann <ogus18@gmail.com>
2014-12-23 11:45:06 +0100

    Cześć dalsza

---
+++ aquadrom/GeneratorNotatek.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace aquadrom
+{
+    partial class GeneratorNotatek
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Generuj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Generuj
+            // 
+            this.Generuj.Location = new System.Drawing.Point(12, 227);
+            this.Generuj.Name = "Generuj";
+            this.Generuj.Size = new System.Drawing.Size(75, 23);
+            this.Generuj.TabIndex = 0;
+            this.Generuj.Text = "Generuj";
+            this.Generuj.UseVisualStyleBackColor = true;
+            this.Generuj.Click += new System.EventHandler(this.Generuj_Click);
+            // 
+            // GeneratorNotatek
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.Generuj);
+            this.Name = "GeneratorNotatek";
+            this.Text = "GeneratorNotatek";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GeneratorNotatek_FormClosing);
+            this.Load += new System.EventHandler(this.GeneratorNotatek_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Generuj;
+    }
+}
---
+++ aquadrom/GeneratorNotatek.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfSharp;
+using PdfSharp.Charting;
+using PdfSharp.Pdf;
+using PdfSharp.Fonts;
+using PdfSharp.Internal;
+using PdfSharp.Drawing;
+using PdfSharp.Forms;
+using PdfSharp.SharpZipLib;
+
+
+
+namespace aquadrom
+{
+    public partial class GeneratorNotatek : Form
+    {
+        public static bool exist = false;
+        public GeneratorNotatek()
+        {
+            InitializeComponent();
+            exist = true;
+        }
+
+        private void GeneratorNotatek_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GeneratorNotatek_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            exist = false;
+        }
+
+        private void Generuj_Click(object sender, EventArgs e)
+        {
+            PdfDocument document = new PdfDocument();
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,
+            new XRect(0, 0, page.Width, page.Height), XStringFormat.Center);
+            string filename = "HelloWorld.pdf";
+            document.Save(filename);
+        }
+    }
+}
