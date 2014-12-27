f9f54d25ccaf3dc89cc017c8710d91b5e15adf94
Ania <lady_anya@o2.pl>
2014-12-27 15:26:27 +0100

    stare zmiany

--- aquadrom/HarmonogramForm.cs
+++ aquadrom/HarmonogramForm.cs
@@ -13,6 +13,8 @@ namespace aquadrom
 {
     public partial class HarmonogramForm : Form
     {
+        private bool loadingFromDB;
+        private bool valueChanged;
         public HarmonogramForm()
         {
             InitializeComponent();
@@ -20,20 +22,31 @@ namespace aquadrom
 
         private void HarmonogramForm_Load(object sender, EventArgs e)
         {
+            loadingFromDB = true;
+            valueChanged = true;
             this.pracownikTableAdapter.Fill(this.aquadromDataSet.Pracownik);
 
             AddDayColumns();
+<<<<<<< HEAD
+            loadingFromDB = false;
+
             listBoxMiesiace.SetSelected(DateTime.Now.Month-1,true);
+
+
+=======
+>>>>>>> 1504433d4c91e8f8772d0feb2b7171936aebae46
             FillFromDB(DateTime.Now);
 
                 // TODO: This line of code loads data into the 'aquadromDataSet.Godziny_pracy' table. You can move, or remove it, as needed.
                 this.godziny_pracyTableAdapter.Fill(this.aquadromDataSet.Godziny_pracy);
-
+           
+            comboBoxMiesiace.SelectedIndex = DateTime.Now.Month - 1;
         }
 
         private void FillFromDB(DateTime date)
         {
-            //Select od do from godziny pracy where id_p =...
+            loadingFromDB = true;
+            
             DBAdapter adapter = new DBAdapter();
             DateTime iDate = new DateTime(date.Year, date.Month, 1);
             ClearHours();
@@ -61,6 +74,8 @@ namespace aquadrom
 
                 iDate = iDate.AddDays(1);
             }
+            loadingFromDB = false;
+
                                    
         }
 
@@ -69,7 +84,6 @@ namespace aquadrom
             for (int j = 0; j < dataGridView1.RowCount - 1; j++)
                 for (int i = 2; i < dataGridView1.ColumnCount; i++)
                     dataGridView1.Rows[j].Cells[i].Value = "";
-                
         }
         private void AddDayColumns()
         {
@@ -79,17 +93,105 @@ namespace aquadrom
             for (int i = 1; i <=  iloscDni; i++)
             {
                 string xyz = i.ToString() + "od" + "  ,  " + i.ToString() + "do";
-               dataGridView1.Columns.Add(i.ToString()+"od", day.ToShortDateString());
-               dataGridView1.Columns.Add(i.ToString()+"do", "");
-               day = day.AddDays(1);
-                    
+                dataGridView1.Columns.Add(i.ToString()+"od", day.ToShortDateString());
+                dataGridView1.Columns.Add(i.ToString()+"do", "");
+                day = day.AddDays(1);
+                DataGridViewColumn column1 = dataGridView1.Columns[i.ToString() + "od"];
+                DataGridViewColumn column2 = dataGridView1.Columns[i.ToString() + "do"];
+                column1.Width = 70;
+                column2.Width = 70;
+                column2.DividerWidth = 1;
+                dataGridView1.Columns[i.ToString() + "od"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                dataGridView1.Columns[i.ToString() + "do"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter; 
+                
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                DataGridViewColumn column = dataGridView1.Columns[i];
+                column.DividerWidth = 1;
+                dataGridView1.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
             }
         }
 
-        private void listBoxMiesiace_SelectedIndexChanged(object sender, EventArgs e)
-        {
 
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+<<<<<<< HEAD
             FillFromDB(new DateTime(DateTime.Now.Year, listBoxMiesiace.SelectedIndex + 1, 1));
         }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            valueChanged = true;
+        }
+
+        private bool CorrectFormat(string value)
+        {
+            DateTime dateValue;
+            if(DateTime.TryParse(value,out dateValue))
+            {
+                MessageBox.Show(dateValue.ToString());
+                return true;
+            }
+            else 
+                return false;
+        }
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            if (!loadingFromDB )
+                if (e.ColumnIndex > 2)
+                {
+                    MessageBox.Show("index ok");
+                    if (valueChanged)
+                    {
+                        MessageBox.Show("xx");
+                        DateTime dateValue;
+                        if (DateTime.TryParse(e.FormattedValue.ToString(), out dateValue))
+                        {
+                            MessageBox.Show(e.FormattedValue.ToString());
+                            dateValue = GetColumnDate(dateValue, e);
+                        }
+                        else
+                            if (e.FormattedValue.ToString().Length > 0)
+                            {
+                                dataGridView1[e.RowIndex, e.ColumnIndex].ErrorText = "Zły format!";
+                                MessageBox.Show("cancel");
+                                e.Cancel = true;
+                            }
+                        valueChanged = false;
+
+                    }
+                }
+
+        }
+
+        private DateTime GetColumnDate(DateTime dateValue, DataGridViewCellValidatingEventArgs e)
+        {
+            string columnName=dataGridView1.Columns[e.ColumnIndex+(e.ColumnIndex%2 -1)].HeaderText;
+            
+            DateTime columnDate;
+            DateTime.TryParse(columnName,out columnDate);
+            MessageBox.Show("col: "+columnName+"  "+columnDate.ToString());
+
+            DateTime newDate = new DateTime(columnDate.Year, columnDate.Month, columnDate.Day, dateValue.Hour, dateValue.Minute, dateValue.Second);
+            MessageBox.Show(newDate.ToString());
+            return newDate;
+            
+            
+        }
+=======
+
+        }
+
+        private void comboBoxMiesiace_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillFromDB(new DateTime(DateTime.Now.Year, comboBoxMiesiace.SelectedIndex + 1, 1));
+        }
+
+
+
+>>>>>>> 1504433d4c91e8f8772d0feb2b7171936aebae46
     }
 }
