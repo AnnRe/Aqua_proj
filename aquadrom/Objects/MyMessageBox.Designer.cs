a4840120685410fb259ceb8c94d4c4a6cf1df943
mateusz.ogiermann <ogus18@gmail.com>
2015-01-26 12:13:10 +0100

    cos tam

---
+++ aquadrom/Objects/MyMessageBox.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace aquadrom.Objects
+{
+    partial class MyMessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyMessageBox));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 124);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Azure;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(12, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tak";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Azure;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(93, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Nie";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Azure;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(174, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Anuluj";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // MyMessageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.BackgroundImage = global::aquadrom.Properties.Resources.bg_page;
+            this.ClientSize = new System.Drawing.Size(260, 189);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MyMessageBox";
+            this.Text = "Aquadrom";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MyMessageBox_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ aquadrom/Objects/MyMessageBox.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aquadrom.Objects
+{
+    public partial class MyMessageBox : Form
+    {
+
+        static MyMessageBox newMessageBox;
+        public Label label;
+        static string Button_id;
+        public DialogResult dialogResult;
+        int disposeFormTimer;
+        MessageBoxButtons buttons;
+
+        public MyMessageBox()
+        {
+            InitializeComponent();
+            label = label1;
+        }
+
+        public static string ShowBox(string txtMessage)
+        {
+
+            newMessageBox = new MyMessageBox();
+            newMessageBox.buttons = MessageBoxButtons.OK;
+            newMessageBox.button2.Text = "OK";
+            newMessageBox.button2.Visible = true;
+            newMessageBox.button1.Visible = false;
+            newMessageBox.button3.Visible = false;
+
+            newMessageBox.label.Text = txtMessage;
+            newMessageBox.ShowDialog();
+            
+            return Button_id;
+        }
+
+        public static DialogResult ShowBox(string txtMessage, string txtTitle)
+        {
+            newMessageBox = new MyMessageBox();
+
+            newMessageBox.buttons = MessageBoxButtons.OK;
+            newMessageBox.button2.Text = "OK";
+            newMessageBox.button2.Visible = true;
+            newMessageBox.button1.Visible = false;
+            newMessageBox.button3.Visible = false;
+            newMessageBox.Text = txtTitle;
+            newMessageBox.label.Text = txtMessage;
+            
+            newMessageBox.ShowDialog();
+            return newMessageBox.dialogResult;
+        }
+
+        public static DialogResult ShowBox(string txtMessage, string txtTitle,MessageBoxButtons messageBoxButtons)
+        {
+            newMessageBox = new MyMessageBox();
+            newMessageBox.buttons = messageBoxButtons;
+
+            if (messageBoxButtons == MessageBoxButtons.OK)
+            {
+                newMessageBox.button2.Text = "OK";
+                newMessageBox.button2.Visible = true;
+                newMessageBox.button1.Visible = false;
+                newMessageBox.button3.Visible = false;
+            }
+            else if(messageBoxButtons==MessageBoxButtons.YesNo)
+            {
+                newMessageBox.button1.Text = "Tak";
+                newMessageBox.button1.Visible = true;
+                newMessageBox.button2.Text = "Nie";
+                newMessageBox.button2.Visible = true;
+                newMessageBox.button3.Visible = false;
+            }
+            else if (messageBoxButtons == MessageBoxButtons.YesNoCancel)
+            {
+                newMessageBox.button1.Text = "Tak";
+                newMessageBox.button1.Visible = true;
+                newMessageBox.button2.Text = "Nie";
+                newMessageBox.button2.Visible = true;
+                newMessageBox.button3.Text = "Anuluj";
+                newMessageBox.button3.Visible = true;
+            }
+            newMessageBox.Text = txtTitle;
+            newMessageBox.label.Text = txtMessage;
+
+            newMessageBox.ShowDialog();
+            return newMessageBox.dialogResult;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             
+            dialogResult = DialogResult.Yes;
+            
+            Button_id = "1";
+            newMessageBox.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (buttons == MessageBoxButtons.OK)
+                dialogResult = DialogResult.OK;
+            else
+                dialogResult = DialogResult.No;
+            Button_id = "2";
+            newMessageBox.Dispose();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dialogResult = DialogResult.Cancel;
+
+            Button_id = "3";
+            newMessageBox.Dispose();
+        }
+
+        private void MyMessageBox_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+        
+    }
+}
